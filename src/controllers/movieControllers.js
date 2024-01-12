1b4bdb583f87940ad92ab482bfbbcfeb910927ac
Jeremy Illien <mr.illien.jeremy@gmail.com>
2024-01-12 12:11:43 +0100

    quest 03 + challenge done

--- src/controllers/movieControllers.js
+++ src/controllers/movieControllers.js
@@ -2,7 +2,7 @@ const database = require("../../database");
 
 const getMovies = (req, res) => {
   database
-    .query("select * from movies")
+    .query("SELECT * FROM movies")
     .then(([movies]) => {
       res.json(movies);
     })
@@ -16,7 +16,7 @@ const getMovieById = (req, res) => {
   const id = parseInt(req.params.id);
 
   database
-    .query("select * from movies where id = ?", [id])
+    .query("SELECT * FROM movies WHERE id = ?", [id])
     .then(([movies]) => {
       if (movies[0] != null) {
         res.json(movies[0]);
@@ -47,8 +47,31 @@ const postMovie = (req, res) => {
     });
 };
 
+const updateMovie = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { title, director, year, color, duration} = req.body;
+
+  database
+  .query(
+    "UPDATE movies SET title = ?, director = ?, year = ?, color = ?, duration = ? WHERE id = ?",
+    [title, director, year, color, duration, id]
+  )
+  .then(([result]) => {
+    if (result.affectedRows === 0) {
+      res.sendStatus(404);
+    } else {
+      res.sendStatus(204);
+    }
+  })
+  .catch((err) => {
+    console.error(err);
+    res.sendStatus(500);
+  });
+};
+
 module.exports = {
   getMovies,
   getMovieById,
   postMovie,
+  updateMovie,
 };
